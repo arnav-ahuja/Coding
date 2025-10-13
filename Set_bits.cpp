6ee3d330a885444b7c418635dbe2aecb1ac066f8
Arnav Ahuja <arnavnsut2029@gmail.com>
2025-10-13 13:11:50 +0530

    Create Set_bits.cpp

---
+++ Set_bits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int bit_calculator(int n){
+    int count=0;
+    while (n!=0)
+    {           
+            if ((n&1)==1)
+            {
+                count+=1;
+            }
+                n=n>>1;       
+    }
+    return count;
+}
+int main(){
+    cout<<"So I am here to calculate set bits , So Less Go"<<endl;
+    int number;
+    cout<<"Enter your number:->";
+    cin>>number;
+    cout<<bit_calculator(number)<<endl;
+}
